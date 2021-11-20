f07dea7558f96158a6c30ce9bbf25b62bc23385e
= <esa.taheresa@gmail.com>
2021-11-20 13:11:02 +0200

    Hienosäätöä

--- dokumentaatio/tuntikirjanpito.md
+++ dokumentaatio/tuntikirjanpito.md
@@ -5,4 +5,4 @@
 | 10.11. | 2    | Tein alustavan vaatimusmäärittelyn ja opiskelin materiaalia
 | 15.11. | 1    | Katsoin jonkin verran Tkinter- ja pylint-moduulien toimintaa
 | 16.11. | 2.5  | Luin lähdemateriaalia ja tein sen perusteella ohjelmalle luokkia
-| 20.11. | 5   | Tkinterin ja sqliten harjoittelua
+| 20.11. | 5    | Tkinterin ja sqliten harjoittelua
--- src/services/intake_trace_service.py
+++ src/services/intake_trace_service.py
@@ -1,10 +1,9 @@
-from entities.totalCalorieIntake import TotalCalorie
-class MouthInput:
+class Intake:
     def __init__(self):
         self.__protein = 0
         self.__carbohydrates = 0
         self.__fat = 0
-        self.totalCal = TotalCalorie(self.set_protein, self.set_carbohydrates, self.set_fat)
+        self.totalCal = self.totalCalorieIntake()
         
     def set_protein(self, protein_gram):
         self.__protein += protein_gram
@@ -31,7 +30,11 @@ class MouthInput:
         return self.__fat
     
     def totalCalorieIntake(self):
-        return self.totalCal(self.get_protein, self.get_carbohydrates, self.get_fat)
+        protein_calories = self.counter.protein_calorie_count(self.__protein)
+        carbohydrate_calories = self.counter.carbohydrates_calorie_count(self.__carbohydrates)
+        fat_calories = self.counter.fat_calorie_count(self.__fat)
+        
+        return protein_calories + carbohydrate_calories + fat_calories        
     
     def __str__(self):
         return f"Protein: {self.__protein}g | Carbohydrates: {self.__carbohydrates}g | Fat: {self.__fat}g"
